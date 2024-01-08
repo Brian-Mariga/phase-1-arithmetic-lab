28b58c2917570344e0c81097f521ae7e0ec820f9
Brian <brianmariga0@gmail.com>
2024-01-08 20:44:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let num1 = Number.parseFloat(8);
+let num2 = Number.parseFloat(7.8);
+const multiply = Math.round(num1 * num2);
+
+const random = Number.parseInt(Math.random()+1);
+console.log(random);
+
+let num3 = Number.parseFloat(19);
+let num4 = Number.parseFloat(5);
+const mod = num3 % num4;
+
+const max = Math.max(5, 10, 15, 20);
+console.log(max);
